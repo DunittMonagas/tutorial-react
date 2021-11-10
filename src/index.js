52eddc570624008802d242eee8ececa5eb30eb76
Dunitt Monagas <dunitt@example.com>
2021-11-10 18:16:43 -0400

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -2,23 +2,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// staless functional component
-// always return JSX
+// JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
 
-function Greeting() {
+/*function Greeting() {
 	return (
 		<div>
 			<h1>Hello World!</h1>
 		</div>
 	);
 }
+*/
 
-/*const Greeting = () => {
-	return React.createElement(
-		'div', 
-		{}, 
-		React.createElement('h1', {}, 'Hello World!')
+function Greeting() {
+	return (
+		<div>
+			<h3>Hello People</h3>
+			<ul>
+				<li>
+					<a href="#">Hello World!</a>
+				</li>
+				<img src='' alt='' />
+				<input type='text' name='' id='' />
+			</ul>
+		</div>
 	);
-};*/
+}
+
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
