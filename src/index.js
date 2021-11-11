3e0af59b31c0bdd23d999b35a215a5aa48aac6bd
Dunitt Monagas <dunitt@example.com>
2021-11-10 21:39:23 -0400

    Key prop and spread operator

--- src/index.js
+++ src/index.js
@@ -8,16 +8,19 @@ import './index.css';
 // setup vars
 const books = [
 	{
+		id: 1, 
 		img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg', 
 		title: 'I Love You to the Moon and Back', 
 		author: 'Amelia Hepworth', 
 	}, 
 	{
+		id: 2, 
 		img: 'https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX384_BO1,204,203,200_.jpg', 
 		title: 'Our Class is a Family', 
 		author: 'Shannon Olsen', 
 	}, 
 	{
+		id: 3, 
 		img: 'https://m.media-amazon.com/images/I/61-ytaG7QmL.jpg', 
 		title: 'Why Nations Fail: The Origins of Power, Prosperity, and Poverty', 
 		author: 'Daron Acemoglu', 
@@ -29,14 +32,14 @@ function BookList() {
 	return (
 		<section className='booklist'>
 			{books.map((book) => {
-				return <Book book={book} />;
+				return <Book key={book.id} {...book} />;
 			})}
 		</section>
 	);
 }
 
-const Book = (props) => {
-	const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+	// const { img, title, author } = props;
 
 	return (
 		<article className='book'>
